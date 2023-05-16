df2a2a57a360e2f227e9291fbf391adee1c9cff5
PrantoshB <pbiswas1007@gmail.com>
2023-05-16 19:42:47 +0530

    Change Readme

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Greeting.create(greeting: "Hello, Prantosh!")
+Greeting.create(greeting: "Hello, Aman!")
+Greeting.create(greeting: "Hello, Hein!")
+Greeting.create(greeting: "Hello, Utsargo!")
+Greeting.create(greeting: "Hello, Salwa!")
